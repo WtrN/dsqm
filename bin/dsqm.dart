3715a970475baff256ae6369afb4ef53ceb1a14d
WtrN <nishi.w.136@gmail.com>
2023-06-04 20:42:16 +0900

    first commit

---
+++ bin/dsqm.dart
@@ -0,0 +1,5 @@
+import 'package:dsqm/dsqm.dart' as dsqm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dsqm.calculate()}!');
+}
---
+++ test/dsqm_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsqm/dsqm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

8bc640904f72feb25e5f37a901a2e044ddcb8224
WtrN <nishi.w.136@gmail.com>
2023-06-16 20:53:41 +0900

    wip

--- bin/dsqm.dart
+++ bin/dsqm.dart
@@ -1,5 +1,66 @@
+import 'package:analyzer/dart/analysis/analysis_context.dart';
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/analysis/features.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:dsqm/dsqm.dart' as dsqm;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dsqm.calculate()}!');
+Future main() async {
+  final file = parseFile(
+      path: '/Users/nishimuraw/projects/scala-git/dsqm/bin/dsqm.dart',
+      featureSet: FeatureSet.latestLanguageVersion());
+
+  if (file.content.isNotEmpty) {
+    print('');
+  }
+  final index = 0;
+
+  switch(index) {
+    case 1 :
+      print('-');
+    case 0 :
+      print('-');
+    case 3:
+      print('-');
+    default:
+      print('-');
+  }
+
+  final visitor = ImportVisitor();
+  file.unit.visitChildren(visitor);
+
+  print(visitor.importedUris);
+  print(visitor.ifStatesIndex);
+}
+
+class ImportVisitor extends RecursiveAstVisitor<void> {
+  final List<String> importedUris = [];
+
+  int ifStatesIndex = 0;
+
+  @override
+  void visitImportDirective(ImportDirective node) {
+    importedUris.add(node.uri.stringValue!);
+    return super.visitImportDirective(node);
+  }
+
+  @override
+  void visitSwitchCase(SwitchCase node) {
+    ifStatesIndex++;
+
+    super.visitSwitchCase(node);
+  }
+
+  @override
+  void visitSwitchPatternCase(SwitchPatternCase node) {
+    ifStatesIndex++;
+    super.visitSwitchPatternCase(node);
+  }
+
+  @override
+  void visitIfStatement(IfStatement node) {
+    ifStatesIndex++;
+    return super.visitIfStatement(node);
+  }
 }
--- lib/dsqm.dart
+++ lib/dsqm.dart
@@ -1,3 +1,7 @@
+import 'package:analyzer/dart/analysis/analysis_context.dart';
+import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer_plugin/plugin/plugin.dart';
+
 int calculate() {
   return 6 * 7;
 }

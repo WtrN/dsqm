3e56a182a92017724bd9cb5d43e964307e32e85d
WtrN <nishi.w.136@gmail.com>
2023-06-18 23:12:18 +0900

    wip

--- bin/dsqm.dart
+++ bin/dsqm.dart
@@ -1,10 +1,8 @@
-import 'package:analyzer/dart/analysis/analysis_context.dart';
-import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:dsqm/dsqm.dart' as dsqm;
+import 'package:dsqm/src/metrics/cyclomatic_complexity_visitor.dart';
 
 Future main() async {
   final file = parseFile(
@@ -27,40 +25,8 @@ Future main() async {
       print('-');
   }
 
-  final visitor = ImportVisitor();
+  final visitor = CyclomaticComplexityVisitor();
   file.unit.visitChildren(visitor);
 
-  print(visitor.importedUris);
-  print(visitor.ifStatesIndex);
-}
-
-class ImportVisitor extends RecursiveAstVisitor<void> {
-  final List<String> importedUris = [];
-
-  int ifStatesIndex = 0;
-
-  @override
-  void visitImportDirective(ImportDirective node) {
-    importedUris.add(node.uri.stringValue!);
-    return super.visitImportDirective(node);
-  }
-
-  @override
-  void visitSwitchCase(SwitchCase node) {
-    ifStatesIndex++;
-
-    super.visitSwitchCase(node);
-  }
-
-  @override
-  void visitSwitchPatternCase(SwitchPatternCase node) {
-    ifStatesIndex++;
-    super.visitSwitchPatternCase(node);
-  }
-
-  @override
-  void visitIfStatement(IfStatement node) {
-    ifStatesIndex++;
-    return super.visitIfStatement(node);
-  }
+  print(visitor.complexityEntities.length);
 }
--- lib/dsqm.dart
+++ lib/dsqm.dart
@@ -1,7 +1,3 @@
-import 'package:analyzer/dart/analysis/analysis_context.dart';
-import 'package:analyzer/file_system/file_system.dart';
-import 'package:analyzer_plugin/plugin/plugin.dart';
-
 int calculate() {
   return 6 * 7;
 }
